693b626dade4d0008cf657295d2fe5bdde2caa23
Yun Seokjun <disclean@naver.com>
2026-09-18 07:32:55 +0900

    사이드패널 접기, 서버의 csv, api키 분리
    - 좌측 사이드패널 열고 닫기 가능
    - 서버에 저장된 csv 파일 로드 가능
    - kakao-sdk-api 키를 별도의 파일에 저장

---
+++ config.js
@@ -0,0 +1,6 @@
+// 카카오 개발자 콘솔(https://developers.kakao.com)에서 발급받은
+// JavaScript 키를 아래 따옴표 안에 넣으세요.
+//
+// 이 파일은 index.html과 분리되어 있으므로, 저장소에 커밋하지 않거나
+// .gitignore에 추가해 키가 공개 저장소에 노출되지 않도록 관리할 수 있습니다.
+const KAKAO_APP_KEY = "40614573984d36782fbe01f8ede47650";
